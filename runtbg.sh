d32c6d2c63c2542402fb127f2c7b02333c4a1465
Chris Babcock <cbabcock@asciiking.com>
2013-11-09 04:05:56 +0000

    Bug 215.3 - Back up TBG source code and data to github

--- runtbg.sh
+++ runtbg.sh
@@ -8,7 +8,7 @@ rm /home/tbg/work/WWW/results/1/battle*
 find /home/tbg/work/WWW/orders/1/ -mtime +7 -exec rm {} \;
 cat /home/tbg/work/tbg/players > /home/tbg/work/data/players
 cat /home/tbg/work/tbg/g1 > /home/tbg/work/data/g1
-cat /home/tbg/work/tbg/data`head -1 g1` > /home/tbg/work/data/data
+cat /home/tbg/work/tbg/data`head -1 /home/tbg/work/tbg/g1` > /home/tbg/work/data/data
 mysqldump main_site --single-transaction > /home/tbg/work/data/main_site
 mysqldump tbg_site > /home/tbg/work/data/tbg_site
 

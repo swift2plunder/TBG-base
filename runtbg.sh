40bd24b646c606374019594ccd0967b3a91a615d
Chris Babcock <cbabcock@asciiking.com>
2013-12-05 15:57:09 +0000

    Bug 233.1 - Keep webroot clean

--- runtbg.sh
+++ runtbg.sh
@@ -4,11 +4,11 @@ export HOSTNAME
 TBG=/home/tbg/work
 export TBG
 rm /home/tbg/work/WWW/results/1/battle*
-/home/tbg/work/bin/tbg -v > /home/tbg/work/tbglog
+/home/tbg/work/bin/tbg > /home/tbg/work/tbglog
 find /home/tbg/work/WWW/orders/1/ -mtime +7 -exec rm {} \;
 cat /home/tbg/work/tbg/players > /home/tbg/work/data/players
 cat /home/tbg/work/tbg/g1 > /home/tbg/work/data/g1
 cat /home/tbg/work/tbg/data`head -1 /home/tbg/work/tbg/g1` > /home/tbg/work/data/data
 mysqldump main_site --single-transaction > /home/tbg/work/data/main_site
 mysqldump tbg_site > /home/tbg/work/data/tbg_site
-
+find -L /home/tbg/work/WWW/ -type l -delete
